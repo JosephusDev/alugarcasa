300126d476a06e99879d24b168d87992706bc972
OliverOliveira <filomenoolivetree@gmail.com>
2024-11-02 16:06:23 +0100

    Eliminar Prorpiedade

--- src/components/PropriedadeComponent.tsx
+++ src/components/PropriedadeComponent.tsx
@@ -6,8 +6,10 @@ import { MeuModal } from "./MeuModal";
 import { Trash } from "lucide-react";
 import { Label } from "./ui/label";
 import { Textarea } from "./ui/textarea";
+import { Button } from "./ui/button";
+import { useToast } from "@/hooks/use-toast";
 
-interface IPropriedades{
+interface IPropriedades {
     id: number;
     descricao: string;
     cidade: string;
@@ -17,6 +19,8 @@ interface IPropriedades{
 }
 
 export default function PropriedadeComponent() {
+
+    const { toast } = useToast();
 
     const [propriedades, setPropriedades] = useState<IPropriedades[]>([]);
     const [search, setSearch] = useState("");
@@ -37,89 +41,106 @@ export default function PropriedadeComponent() {
         }
     };
 
+    const deletePropriedade = async (id: number) => {
+        const token = localStorage.getItem('token');
+        try {
+            const response = await Api.delete(`/propriedade/${id}`, {
+                headers: { 'Authorization': `Bearer ${token}` }
+            });
+
+            const resposta = response.data.message;
+            toast({
+                description: resposta
+            });
+
+            setPropriedades(prevState => prevState.filter(prop => prop.id !== id));
+        } catch (error) {
+            console.error("Erro ao excluir propriedade:", error);
+            toast({ 
+                description: "Erro ao excluir propriedade"
+            });
+        }
+    }
+
     useEffect(() => {
         getPropriedades();
     }, []);
 
-    const filteredPropriedades = search ? propriedades?.filter((p)=>p.descricao.toLowerCase().includes(search.toLowerCase())) : propriedades
-    
+    const filteredPropriedades = search ? propriedades?.filter((p) => p.descricao.toLowerCase().includes(search.toLowerCase())) : propriedades;
 
     return (
-        <>
-            <div className="m-4 p-4 gap-4">
-                <div className="gap-4 m-4 p-4">
-                    <div className="my-5">
-                        <h1 className="text-3xl font-bold">Gerencie suas propriedades</h1>
+        <div className="m-4 p-4 gap-4">
+            <div className="gap-4 m-4 p-4">
+                <div className="my-5">
+                    <h1 className="text-3xl font-bold">Gerencie suas propriedades</h1>
+                </div>
+                <div className="flex justify-between items-center gap-4">
+                    <div>
+                        <Input onChange={(e) => setSearch(e.target.value)} type="text" placeholder="Pesquisar" />
                     </div>
-                    <div className="flex justify-between items-center gap-4">
-                        <div>
-                            <Input onChange={(e)=>setSearch(e.target.value)} type="text" placeholder="Pesquisar" />
-                        </div>
-                        <div>
-                            <MeuModal 
-                                buttonTitle="Adicionar"
-                                children={
-                                    <>
-                                        <div className="grid grid-rows-1 gap-1">
-                                            <Label htmlFor="descricao">Descrição da casa</Label>
-                                            <Textarea id="descricao"  className="col-span-3"/>
-                                        </div>
-                                        <div className="grid grid-rows-1 gap-1">
-                                            <Label htmlFor="cidade">Cidade</Label>
-                                            <Input id="cidade" className="col-span-3" />
-                                        </div>
-                                        <div className="grid grid-rows-1 gap-1">
-                                            <Label htmlFor="bairro">Bairro</Label>
-                                            <Input id="bairro" className="col-span-3" />
-                                        </div>
-                                        <div className="grid grid-rows-1 gap-1">
-                                            <Label htmlFor="preco">Preço</Label>
-                                            <Input type="number" id="preco" className="col-span-3" />
-                                        </div>
-                                        <div className="grid grid-rows-1 gap-1">
-                                            <Label htmlFor="imagem">Imagem</Label>
-                                            <Input type="file" id="imagem" className="col-span-3" />
-                                        </div>
-                                    </>
-                                }
-                            />
-                        </div>
-                    </div>
-
-                    <div className="pt-10">
-                        <Table>
-                            <TableHeader>
-                                <TableRow>
-                                    <TableHead>#</TableHead>
-                                    <TableHead>Descrição</TableHead>
-                                    <TableHead>Cidade</TableHead>
-                                    <TableHead>Bairro</TableHead>
-                                    <TableHead>Preço</TableHead>
-                                    <TableHead>Imagem</TableHead>
-                                    <TableHead>Ações</TableHead>
-                                </TableRow>
-                            </TableHeader>
-                            <TableBody>
-                                {filteredPropriedades.length > 0 ? filteredPropriedades.map((prop, index) => (
-                                    <TableRow key={prop.id}>
-                                        <TableCell>{index + 1}</TableCell>
-                                        <TableCell>{prop.descricao}</TableCell>
-                                        <TableCell>{prop.cidade}</TableCell>
-                                        <TableCell>{prop.bairro}</TableCell>
-                                        <TableCell>{prop.preco}</TableCell>
-                                        <TableCell>{prop.imagem}</TableCell>
-                                        <TableCell>{<Trash size={15} />}</TableCell>
-                                    </TableRow>
-                                )) : (
-                                    <TableRow>
-                                        <TableCell className="text-center py-5 font-bold" colSpan={7}><h1 className="text-lg">Nenhuma propriedade encontrada</h1></TableCell>
-                                    </TableRow>
-                                )}
-                            </TableBody>
-                        </Table>
+                    <div>
+                        <MeuModal
+                            buttonTitle="Adicionar"
+                            children={
+                                <>
+                                    <div className="grid grid-rows-1 gap-1">
+                                        <Label htmlFor="descricao">Descrição da casa</Label>
+                                        <Textarea id="descricao" className="col-span-3" />
+                                    </div>
+                                    <div className="grid grid-rows-1 gap-1">
+                                        <Label htmlFor="cidade">Cidade</Label>
+                                        <Input id="cidade" className="col-span-3" />
+                                    </div>
+                                    <div className="grid grid-rows-1 gap-1">
+                                        <Label htmlFor="bairro">Bairro</Label>
+                                        <Input id="bairro" className="col-span-3" />
+                                    </div>
+                                    <div className="grid grid-rows-1 gap-1">
+                                        <Label htmlFor="preco">Preço</Label>
+                                        <Input type="number" id="preco" className="col-span-3" />
+                                    </div>
+                                    <div className="grid grid-rows-1 gap-1">
+                                        <Label htmlFor="imagem">Imagem</Label>
+                                        <Input type="file" id="imagem" className="col-span-3" />
+                                    </div>
+                                </>
+                            }
+                        />
                     </div>
                 </div>
+                <div className="pt-10">
+                    <Table>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead>#</TableHead>
+                                <TableHead>Descrição</TableHead>
+                                <TableHead>Cidade</TableHead>
+                                <TableHead>Bairro</TableHead>
+                                <TableHead>Preço</TableHead>
+                                <TableHead>Imagem</TableHead>
+                                <TableHead>Ações</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {filteredPropriedades.length > 0 ? filteredPropriedades.map((prop, index) => (
+                                <TableRow key={prop.id}>
+                                    <TableCell>{index + 1}</TableCell>
+                                    <TableCell>{prop.descricao}</TableCell>
+                                    <TableCell>{prop.cidade}</TableCell>
+                                    <TableCell>{prop.bairro}</TableCell>
+                                    <TableCell>{prop.preco}</TableCell>
+                                    <TableCell>{prop.imagem}</TableCell>
+                                    <TableCell><Button variant={"outline"} onClick={() => deletePropriedade(prop.id)}><Trash /></Button></TableCell>
+                                </TableRow>
+                            )) : (
+                                <TableRow>
+                                    <TableCell className="text-center py-5 font-bold" colSpan={7}><h1 className="text-lg">Nenhuma propriedade encontrada</h1></TableCell>
+                                </TableRow>
+                            )}
+                        </TableBody>
+                    </Table>
+                </div>
             </div>
-        </>
+        </div>
     );
 }
