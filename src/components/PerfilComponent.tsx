ac82a1b3659b288a0d1e0f259dc9991c4cdcf65b
Bênção Nkazi Toko <bencaotoko2021@gmail.com>
2024-11-10 13:30:20 +0100

    Integração com API para update de perfil no Front end

--- src/components/PerfilComponent.tsx
+++ src/components/PerfilComponent.tsx
@@ -4,10 +4,38 @@ import { Button } from './ui/button'
 import { SidebarMenuButton } from './ui/sidebar'
 import { Label } from './ui/label'
 import { Input } from './ui/input'
-
-const nome = localStorage.getItem('nome')
+import { toast, useToast } from '@/hooks/use-toast'
+import { useState } from 'react'
+import Api from '@/api'
 
 export function PerfilComponent() {
+  const nomeIncial = localStorage.getItem('nome')
+  const [nome, setNome] = useState(nomeIncial || '')
+  const [senha, setSenha] = useState('')
+
+  const editarPerfil = async () => {
+    const id_usuario = localStorage.getItem('id')
+    const token = localStorage.getItem('token')
+
+    try {
+      const response = await Api.put(
+        `/usuario/editar/${id_usuario}`,
+        { nome, senha },
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        },
+      )
+      toast({
+        description: 'Perfil atualizado com sucesso',
+      })
+    } catch (error) {
+      toast({
+        description: 'Erro ao editar perfil',
+        variant: 'destructive',
+      })
+    }
+  }
+  //
   return (
     <>
       <MeuModal
@@ -27,16 +55,24 @@ export function PerfilComponent() {
               <Label htmlFor='nome'>Nome de Usuário</Label>
               <Input
                 id='nome'
-                value={nome?.toString()}
+                value={nome}
+                onChange={(e) => setNome(e.target.value)}
                 className='col-span-3'
               />
             </div>
             <div className='grid grid-rows-1 gap-1'>
               <Label htmlFor='senha'>Senha</Label>
-              <Input id='senha' className='col-span-3' />
+              <Input
+                id='senha'
+                type='password'
+                value={senha}
+                onChange={(e) => setSenha(e.target.value)}
+                className='col-span-3'
+              />
             </div>
           </>
         }
+        onClick={editarPerfil}
       />
     </>
   )
