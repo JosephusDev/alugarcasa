e53aad20f65e2e8ee95f4b892b63919eb4d1558f
OliverOliveira <filomenoolivetree@gmail.com>
2024-11-16 15:09:26 +0100

    Atualização
    Criação do redirecionamento quando token expira

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,46 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 import LoginScreen from '@/pages/Login'
 import CadastroScreen from '@/pages/Cadastro'
 import { PropriedadeComponent } from '@/components/PropriedadeComponent'
 import { HomeComponent } from '@/components/HomeComponent'
 import { useAuth } from '@/context/AuthContext'
+import Api from '@/api'
 
 export default function Rotas() {
-  const { isAuthenticated } = useAuth()
+  const { isAuthenticated, logout } = useAuth()
+  const [loading, setLoading] = useState(true) // Estado de carregamento
+  const token = localStorage.getItem('token')
+
+  useEffect(() => {
+    const verifyToken = async () => {
+      try {
+        const response = await Api.get('/verify-token', {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+        if (!response.data.valide) {
+          logout()
+        }
+      } catch (error) {
+        console.error('Erro ao verificar token:', error)
+        logout()
+      } finally {
+        setLoading(false) // Finaliza o carregamento
+      }
+    }
+
+    if (token) {
+      verifyToken()
+    } else {
+      setLoading(false) // Nenhum token, carregamento é concluído
+    }
+  }, [token, logout])
+
+  if (loading) {
+    return <></> // Renderiza uma tela de carregamento
+  }
+
   return (
     <Routes>
       <Route
